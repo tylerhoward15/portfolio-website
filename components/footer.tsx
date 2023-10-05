7e401e6c562e6265985c668e5d107a7e755ff772
Tyler Howard <tyler98ky@gmail.com>
2023-10-05 13:39:25 -0700

    make copyright dynamically update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+const date = new Date();
 
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Tyler. All rights reserved.
+        &copy; {date.getFullYear()} Tyler. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with

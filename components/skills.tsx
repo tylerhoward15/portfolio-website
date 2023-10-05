9a544962622ff675072502184aa4828947cc2870
Tyler Howard <tyler98ky@gmail.com>
2023-10-05 14:39:44 -0700

    fix navbar animation thresholds

--- components/skills.tsx
+++ components/skills.tsx
@@ -21,7 +21,7 @@ const fadeInAnimationVariants = {
 };
 
 export default function Skills() {
-  const { ref } = useSectionInView("Skills");
+  const { ref } = useSectionInView("Skills", 1);
 
   return (
     <section
